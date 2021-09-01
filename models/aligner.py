532833e8cd008f527ac0c9a6973eee67e4b366ee
bwt09 <tanbowen@sjtu.edu.cn>
2021-09-01 15:08:22 -0400

    updated.

--- models/aligner.py
+++ models/aligner.py
@@ -1,3 +1,9 @@
+from nltk.corpus import stopwords
+
+
+en_stopwords = stopwords.words('english')
+
+
 class Aligner:
     def __init__(self, aggr_type):
         self._aggr_type = aggr_type
@@ -5,13 +11,20 @@ class Aligner:
     def align(self, input_text, context):
         raise NotImplementedError
 
-    def aggregate(self, tokens, token_scores):
+    def aggregate(self, tokens, token_scores, ban_stopwords):
         assert len(tokens) == len(token_scores)
 
+        scores_to_aggr = []
+        for token, score in zip(tokens, token_scores):
+            if (token.lower() not in en_stopwords) or ban_stopwords == False:
+                scores_to_aggr.append(score)
+
+
         if self._aggr_type == 'mean':
-            return sum(token_scores) / len(token_scores)
+            return sum(scores_to_aggr) / len(scores_to_aggr) \
+                if len(scores_to_aggr) > 0 else 0.
         elif self._aggr_type == 'sum':
-            return sum(token_scores)
+            return sum(scores_to_aggr)
         else:
             raise ValueError
 
