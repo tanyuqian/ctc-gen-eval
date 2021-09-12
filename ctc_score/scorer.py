dbf8e62a12de7bea7e3d2bdaf4509bd3be12a2f5
bwt09 <tanbowen@sjtu.edu.cn>
2021-09-12 19:05:57 -0400

    updated.

--- ctc_score/scorer.py
+++ ctc_score/scorer.py
@@ -9,12 +9,11 @@ from ctc_score.models.bert_aligner import BERTAligner
 class Scorer:
     def __init__(self, dataset, aligner, aligner_configs=None):
         self._dataset = dataset
+        self._aligner = aligner
+        self._aligner_configs = aligner_configs
 
-        if aligner not in ['E', 'D', 'R']:
-            self._aligner = aligner
-        else:
-            self._aligner_configs = \
-                set_aligner_configs(dataset, aligner, aligner_configs)
+        if isinstance(self._aligner, str):
+            self._fix_aligner_configs()
 
             if aligner == 'D':
                 pass
@@ -23,22 +22,21 @@ class Scorer:
             elif aligner == 'R':
                 pass
 
+    def _fix_aligner_configs(self):
+        if self._aligner_configs is None:
+            self._aligner_configs = {}
+
+        if 'aggr_type' not in self._aligner_configs and \
+                self._dataset in ['persona_chat', 'topical_chat']:
+            self._aligner_configs['aggr_type'] = 'sum'
+
+        default_configs = json.load(
+            open(f'{ctc_score.__path__}/default_configs.json'))
+        for key, value in default_configs[self._aligner].items():
+            if key not in self._aligner_configs:
+                self._aligner_configs[key] = value
+
     def score(self, *args, **kwargs):
         raise NotImplementedError
 
 
-def set_aligner_configs(dataset, aligner, aligner_configs):
-    if aligner_configs is None:
-        aligner_configs = {}
-
-    default_configs = json.load(
-        open(f'{ctc_score.__path__}/default_configs.json'))
-    for key, value in default_configs[aligner].items():
-        if key not in aligner_configs:
-            aligner_configs[key] = value
-
-    if 'aggr_type' not in aligner_configs:
-        aligner_configs['aggr_type'] = \
-            'sum' if dataset in ['persona_chat', 'topical_chat'] else 'mean'
-
-    return aligner_configs
